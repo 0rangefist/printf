70773791bae4a4e2567db8bc8adb054999a08b32
Thomas <collinskthomas@gmail.com>
2023-03-27 22:09:52 +0000

    Fix bug (_printf.c)
    - Fixes a bug in "_printf.c" that caused the printing of certain combinations of "\" and "%" to come out wrong.
    - Removes the helper function "print_escape_sequence" as part of the bug fix - simplifying the code.

--- _printf.c
+++ _printf.c
@@ -1,60 +1,36 @@
 #include "main.h"
-
 /**
  * print_specified_data - Prints an argument from a list
  * of arguments in a way determined by a given specifier
  *
- * @specifier_char: The specifier to determine how
- * printing should be done
- *
  * @args: The list of arguments from which our
  * argument of interest can be found
  *
+ * @format_ptr_ptr: The ptr to the format string ptr
+ *
  * Return: The number of characters printed
  */
-int print_specified_data(va_list args, const char specifier_char)
+int print_specified_data(va_list args, const char **format_ptr_ptr)
 {
 	/* check if the specifier_char matches any pre-defined */
-	switch (specifier_char)
+	/* value of the next char in the format string */
+	switch (*(*(format_ptr_ptr) + 1))
 	{
 		case 'c':
+			/* dereference format_ptr & increment it by 1 */
+			/* this moves format_ptr to the next character */
+			*format_ptr_ptr = *(format_ptr_ptr) + 1;
 			return (print_char(args));
 		case 's':
+			*format_ptr_ptr = *(format_ptr_ptr) + 1;
 			return (print_string(args));
 		case 'd':
 		case 'i':
+			*format_ptr_ptr = *(format_ptr_ptr) + 1;
 			return (print_integer(args));
-		case '%':
-			return (write(STDOUT_FILENO, "%", 1));
 		default:
-			/* print the % char & unidentified specifier_char */
-			return (write(STDOUT_FILENO, "%", 1)
-				   + write(STDOUT_FILENO, &specifier_char, 1));
-	}
-}
-
-/**
- * print_escape_sequence - Prints an escape sequence
- *
- * @special_char: The special character that determines
- * what the escape sequence is
- *
- * Return: The number of characters printed
- */
-int print_escape_sequence(const char special_char)
-{
-	/* check if the special_char matches any pre-defined */
-	switch (special_char)
-	{
-		case 't': /* tab character */
-			return (write(STDOUT_FILENO, "\t", 1));
-		case 'r': /* carriage return character */
-			return (write(STDOUT_FILENO, "\r", 1));
-		case 'n': /* newline character */
-			return (write(STDOUT_FILENO, "\n", 1));
-		default:
-			/* print the unidentified special_char */
-			return (write(STDOUT_FILENO, &special_char, 1));
+			/* when no specifier char */
+			return (write(STDOUT_FILENO, "%%", 1));
 	}
 }
 
@@ -69,11 +45,9 @@ int print_escape_sequence(const char special_char)
 int _printf(const char *format, ...)
 {
 	va_list args;
-	char	specifier_char;
-	char	special_char;
 	/* pointer to the current character in the format string */
-	const char *format_ptr		= format;
-	int	  num_chars_printed = 0;
+	const char *format_ptr		  = format;
+	int			num_chars_printed = 0;
 
 	if (!format) /* check if format is NULL pointer */
 	{
@@ -88,21 +62,27 @@ int _printf(const char *format, ...)
 		switch (*format_ptr)
 		{
 			case '%': /* if the format specifier % is encountered */
-				/* save the next character after the specifier */
-				specifier_char = *(++format_ptr);
-				num_chars_printed += print_specified_data(args, specifier_char);
+				if (*(format_ptr + 1) == '%') /* if next char == % */
+				{
+					/*  print % */
+					num_chars_printed += write(STDOUT_FILENO, format_ptr, 1);
+					format_ptr++;
+					break;
+				}
+				/* we have a potential specifier character */
+				num_chars_printed += print_specified_data(args, &format_ptr);
 				break;
-			case '\\': /* if the escape character is encountered */
-				/* save the next character after the escape char */
-				special_char = *(++format_ptr);
-				num_chars_printed += print_escape_sequence(special_char);
-				break;
-			default: /* if any ordinary character is encountered */
+			default: /* if any other character is encountered */
 				num_chars_printed += write(STDOUT_FILENO, format_ptr, 1);
 				break;
 		}
 		format_ptr++;
 	}
 	va_end(args);
+	if (args == NULL)
+	{
+		return (-1);
+	}
 	return (num_chars_printed);
 }
+
